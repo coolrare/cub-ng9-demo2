ebfa2e1aa11b0f82b25a155849e231e1dc18563a
Will <doggy.huang@gmail.com>
2020-06-10 19:04:39 +0800

    加入 Tables 元件並設定路由與路由連結

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { TablesComponent } from './tables/tables.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
+  { path: 'tables', component: TablesComponent },
   { path: 'page1', component: Page1Component },
   { path: 'page2', component: Page2Component }
 ];
