e63c932bd8c4e0a846a16630c1799e9e80129a20
Will <doggy.huang@gmail.com>
2020-06-15 21:17:12 +0800

    示範自訂身分證字號檢查功能
    npm i -S taiwan-id-validator

--- src/app/login2/login2.component.ts
+++ src/app/login2/login2.component.ts
@@ -1,6 +1,23 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { LoginViewModel } from '../login/login.component';
-import { NgForm, FormGroup, FormBuilder, Validators, FormGroupDirective } from '@angular/forms';
+import { NgForm, FormGroup, FormBuilder, Validators, FormGroupDirective, FormControl } from '@angular/forms';
+import {
+  isGuiNumberValid, // 統一編號
+  isNationalIdentificationNumberValid, // 身分證字號
+  isResidentCertificateNumberValid, // 居留證編號
+  isCitizenDigitalCertificateValid, // 自然人憑證
+  isEInvoiceCellPhoneBarcodeValid, // 手機條碼
+  isEInvoiceDonateCodeValid // 捐贈碼
+} from 'taiwan-id-validator'
+
+function isValidTaiwanId(control: FormControl) {
+  if (isNationalIdentificationNumberValid(control.value)) {
+    return null;
+  } else {
+    return { isNationalIdentificationNumberValid: true };
+  }
+}
+
 
 @Component({
   selector: 'app-login2',
@@ -27,7 +44,7 @@ export class Login2Component implements OnInit, OnDestroy {
 
     this.form = this.fb.group({
       email: ['', [
-        Validators.required, Validators.email
+        Validators.required, isValidTaiwanId
       ]],
       password: ['', [
         Validators.required, Validators.minLength(3)
