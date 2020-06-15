0f955d6a3d13d06f60489e621166de54737528da
Will <doggy.huang@gmail.com>
2020-06-15 13:56:36 +0800

    加入 PreloadAllModules 預載機制

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -30,7 +30,8 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    useHash: true
+    useHash: true,
+    preloadingStrategy: PreloadAllModules
   })],
   exports: [RouterModule]
 })
