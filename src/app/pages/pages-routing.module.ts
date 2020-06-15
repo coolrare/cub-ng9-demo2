97557b82a708116be4bc89ef3bd174e1ea1bfcd3
Will <doggy.huang@gmail.com>
2020-06-15 13:27:53 +0800

    移除 PagesComponent

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { PagesComponent } from './pages.component';
-
 const routes: Routes = [
-  { path: '', component: PagesComponent }
 ];
 
 @NgModule({
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PagesRoutingModule } from './pages-routing.module';
-import { PagesComponent } from './pages.component';
 
 
 @NgModule({
-  declarations: [PagesComponent],
+  declarations: [
+
+  ],
   imports: [
     CommonModule,
     PagesRoutingModule
