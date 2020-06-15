8363a05ce81ba6de3695937f434b44e42d105509
Will <doggy.huang@gmail.com>
2020-06-15 13:33:19 +0800

    加入 BlankComponent 到 PagesModule 中，並驗證延遲載入能力

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlankComponent } from './blank/blank.component';
 
 const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'blank' },
+  { path: 'blank', component: BlankComponent }
 ];
 
 @NgModule({
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PagesRoutingModule } from './pages-routing.module';
+import { BlankComponent } from './blank/blank.component';
 
 
 @NgModule({
   declarations: [
-
+    BlankComponent
   ],
   imports: [
     CommonModule,
